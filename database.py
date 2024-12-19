2ca062e2c0215c8936dcd35f1113bb24be08fe14
bsabio <bs679@njit.edu>
2024-12-19 17:21:28 -0500

    docker updated and webpage and styles created

--- database.py
+++ database.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:password@db:5432/calculator_db"
+DATABASE_URL = "postgresql+psycopg2://postgres:password@postgres:5432/calculator_db"
+# Set up database engine
 engine = create_engine(DATABASE_URL)
+# Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base class for models
 Base = declarative_base()
 
-# Create tables (if they don't already exist)
-Base.metadata.create_all(bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
